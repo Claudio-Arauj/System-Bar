754b3cd303cf9d8a9e5c147c4cf86ce3aa3e8d62
Cláudio Pereira <claudiopereira2004@gmail.com>
2023-09-15 21:09:43 -0300

    PyBar V0.5 - Serando telas entre arquivos.

---
+++ Clientes.h
@@ -0,0 +1,9 @@
+
+// Assinatura das Funcoes do menu cliente
+
+void menu_cliente(void);
+void tela_pedidos(void);
+void tela_comidas(void);
+void tela_bebidas(void);
+void tela_visualizar_pedido(void);
+void tela_ajuda(void);
---
+++ Gerencia.h
@@ -0,0 +1,11 @@
+
+// Assinatura das Funcoes do menu gerencia
+
+void acesso_gerencia(void);
+void menu_cadastramento(void);
+void menu_login(void);
+void menu_gerencia(void);
+void tela_pedidos_gerencia(void);
+void tela_caixa(void);
+void tela_relatorio(void);
+void tela_funcionarios(void);
