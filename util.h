1518cd35639f71b9783c0710e64ddefb8d1df3e6
Cláudio Pereira <claudiopereira2004@gmail.com>
2023-10-05 18:13:15 -0300

    PyBar V0.8 - Adição da validação de CPF

--- util.h
+++ util.h
@@ -1,6 +1,14 @@
 
 // Assinatura das Funcoes de utilidade geral
 
+// validar nomes
 int valida_nome(char *nome);
 int eh_letra_acentuada(char c);
 void le_nome(char *nome);
+
+// validar cpf
+int validarCPF(char *cpf);
+void le_cpf(char *cpf);
+
+// funcoes uteis
+void limpar_buffer();
